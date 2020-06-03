99f6b00d4378d5866ecbad40d38477cb507543c5
Billy Ferrara <willfira717@gmail.com>
2020-06-03 22:17:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,6 +10,7 @@ class Owner
     @name = name
     @species = "human"
     @@all << self
+    @cats = []
 
   end
 
@@ -29,8 +30,7 @@ class Owner
     @@all.clear
   end
 
-  def cats
-  end 
+
 
 
 end
