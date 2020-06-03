68933a0bc463e36f9daf4e49cbcdde4fa3947db1
Billy Ferrara <willfira717@gmail.com>
2020-06-03 18:10:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,14 @@
 class Owner
-    attr_reader :name
+    attr_reader :name, :species
     attr_accessor
 
-  def initialize(name)
+  def initialize(name, species)
     @name = name
+    @species = species
   end
 
   def species
-  end 
+
+  end
 
 end
