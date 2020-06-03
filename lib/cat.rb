c8f9124e0ba31702357e17ac19a4f98128f91030
Billy Ferrara <willfira717@gmail.com>
2020-06-03 23:51:14 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,18 +3,14 @@ class Cat
   attr_reader :name
   attr_accessor :mood, :owner,
 
-  @@all = []
 
-  def self.all
-    @@all
-  end
 
 
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
-    @@all << self
+  
   end
 
 
