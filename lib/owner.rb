730d26facfac05d360691b6c8f50cf1db865fbbe
Billy Ferrara <willfira717@gmail.com>
2020-06-04 00:20:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ class Owner
     def sell_pets
       pets.each do |species, animals|
         animals.each do |animal|
-          animal.mood = "nervous"
+          animals.mood = "nervous"
             end
             animals.clear
           end
